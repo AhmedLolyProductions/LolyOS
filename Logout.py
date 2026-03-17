a26fe1c04250d4f172ce576d605e80666809c597
Ahmed Loly Productions <poopdog123@tutamail.com>
2026-03-17 11:06:53 -0400

    Create Logout

---
+++ Logout.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+print("\n" * 41)
+print("Shutting down")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print("Please restart the machine to startup the OS...")
+sys.exit()
