ce0af471540d19cd4cb9172945e54763ba7c1a86
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-04-28 09:06:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 from time import sleep
+
+def BIOS():
+    def CreditsAndVersion():
+        sleep(1)
+        print("github.com/AhmedLolyProductions")
+        sleep(0.5)
+        print("LolyOS v0.0.0-alpha")
+        sleep(2)
+        Boot()
+    def ReBoot():
+        sleep(1)
+        print("Invalid Input")
+        sleep(0.8)
+        Boot()
+    def Boot():
+        print("\n" * 2, "Boot into 'LolyOS'? [Y/N]")
+        User_Input = input("admin:/ $ ").lower()
+        if User_Input in ['y', 'yes']:
+            import Startup
+        elif User_Input in ['n', 'no']:
+            import Shutdown
+        else:
+            ReBoot()
+    CreditsAndVersion()
+BIOS()
