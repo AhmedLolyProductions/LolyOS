47eac25ec68ebf26a9d93806aeaecf7c5015aa75
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-04-29 09:13:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,8 @@ def BIOS():
         sleep(0.5)
         print("LolyOS v0.0.0-alpha")
         sleep(2)
-        Boot()
-    def ReBoot():
-        sleep(1)
-        print("Invalid Input")
-        sleep(0.8)
-        Boot()
-    def Boot():
-        print("\n" * 2, "Boot into 'LolyOS'? [Y/N]")
-        User_Input = input("admin:/ $ ").lower()
-        if User_Input in ['y', 'yes']:
-            import Startup
-        elif User_Input in ['n', 'no']:
-            import Shutdown
-        else:
-            ReBoot()
+        import Startup
     CreditsAndVersion()
-
 # This block of code is from GitHub Copilot (apparently GitHub has issues with the 'input()' function)
 if __name__ == "__main__":
     BIOS()
