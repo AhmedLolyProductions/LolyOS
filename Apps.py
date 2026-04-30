5f1fc551aefe13a6b93892bacda8e5bc0b16ab77
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-04-30 11:04:24 -0400

    Fix indentation errors in cmd() function

--- Apps.py
+++ Apps.py
@@ -3,13 +3,13 @@ import sys
 
 def App_List():
     print("Choose an app or function to execute")
-    sleep(0.5)
+    sleep(0.3)
     print("Shutdown")
-    sleep(0.5)
+    sleep(0.3)
     print("About LolyOS")
-    sleep(0.5)
+    sleep(0.3)
     print("cmd.exe")
-    sleep(0.5)
+    sleep(0.3)
     print("virus.exe")
     User_Input = input("admin:/ ").lower()
     if User_Input == 'shutdown':
@@ -30,7 +30,7 @@ def About_LolyOS():
     sleep(3)
     print("The old version of LolyOS that's archived is github.com/AhmedLolyProductions/LolyOS-old")
     sleep(3)
-    print("LolyOS v1.0.0")
+    print("LolyOS v1.0.2")
     sleep(0.6)
     print("To go back to the app list, click 'enter'")
     input()
@@ -46,28 +46,28 @@ def cmd():
     print("Type 'help' for a list of commands")
     while True:
         Command_Input = input("admin:/ $ ").lower()
-    if Command_Input == 'help':
-        print("\n")
-        sleep(0.2)
-        print("RMDIR: delete a directory")
-        sleep(0.5)
-        print("dir: display directorys")
-        sleep(0.5)
-        print("cls: clears the screen")
-    elif Command_Input == 'rmdir':
-        print("\n" + "please choose a directory to delete; for a list of directorys, type 'dir'")
-    elif Command_Input == 'cls':
-        print("\n" * 41)
-    elif Command_Input == 'dir':
-        print("\n" + "folder: LolyOS")
-        sleep(0.2)
-        print("folder: system")
-        sleep(0.2)
-        print("folder: user")
-        sleep(0.2)
-        print("folder: admin")
-        sleep(0.2)
-        print("file: virus.exe")
-    elif Command_Input in ['rmdir lolyos', 'rmdir system', 'rmdir user', 'rmdir admin', 'rmdir virus.exe', 'rmdir virus']:
-        import Virus
+        if Command_Input == 'help':
+            print("\n")
+            sleep(0.2)
+            print("RMDIR: delete a directory")
+            sleep(0.5)
+            print("dir: display directorys")
+            sleep(0.5)
+            print("cls: clears the screen")
+        elif Command_Input == 'rmdir':
+            print("\n" + "please choose a directory to delete; for a list of directorys, type 'dir'")
+        elif Command_Input == 'cls':
+            print("\n" * 41)
+        elif Command_Input == 'dir':
+            print("\n" + "folder: LolyOS")
+            sleep(0.2)
+            print("folder: system")
+            sleep(0.2)
+            print("folder: user")
+            sleep(0.2)
+            print("folder: admin")
+            sleep(0.2)
+            print("file: virus.exe")
+        elif Command_Input in ['rmdir lolyos', 'rmdir system', 'rmdir user', 'rmdir admin', 'rmdir virus.exe', 'rmdir virus']:
+            import Virus
 App_List()
