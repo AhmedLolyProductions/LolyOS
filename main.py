47e022dc0ca638e66d1c689dd9ea16aada40a6e4
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-05-12 11:52:39 -0400

    Simplify the code

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from time import sleep
 
-def BIOS():
-    def CreditsAndVersion():
-        sleep(1)
-        print("github.com/AhmedLolyProductions")
-        sleep(0.5)
-        print("LolyOS v1.0.2")
-        sleep(2)
-        import Startup
-    CreditsAndVersion()
+sleep(1)
+print("github.com/AhmedLolyProductions")
+sleep(0.5)
+print("LolyOS v1.0.3")
+sleep(2)
+import Startup
