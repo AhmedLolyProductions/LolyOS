58bad9e6dfbece25076542497e502516f3c63d56
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-05-15 10:28:47 -0400

    Update LolyOS version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
   sleep(1)
   print("github.com/AhmedLolyProductions")
   sleep(0.5)
-  print("LolyOS v1.1.3")
+  print("LolyOS v1.1.5")
   sleep(2)
   import Startup
   Startup.Startup()
