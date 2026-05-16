9ea596225fc2ae2d27a495ff1798f3e6e2522c97
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-05-16 19:52:14 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-# GitHub Copilot added this (apparently GitHub actions is more complicated than it seems)
-from .main import BIOS
+from .main import main
 if __name__ == "__main__":
-    BIOS()
+    main()
