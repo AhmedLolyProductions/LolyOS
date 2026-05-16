451a8645bf0787bb77ce56f465e63b14215d63b7
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-05-16 19:54:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from time import sleep
 
+sleep(1)
+print("github.com/AhmedLolyProductions")
+sleep(0.5)
+print("LolyOS v1.1.5")
+sleep(2)
+
 if __name__ == "__main__":
-    sleep(1)
-    print("github.com/AhmedLolyProductions")
-    sleep(0.5)
-    print("LolyOS v1.1.5")
-    sleep(2)
     import Startup
